04e0c1a99673c0ce3066ec2e618bc3a9018486a5
Peter Fry <peterfry@peters-mbp-2.mynet>
2019-05-30 18:35:17 +0100

    fixing health check and postgresql as adapter

--- bin/check_health.sh
+++ bin/check_health.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ -z "${SECURE_USERNAME}" ]]; then
-  exec curl --fail http://localhost:3000/
+  exec curl --fail http://localhost:3000/environments
 else
-  exec curl --user $SECURE_USERNAME:$SECURE_PASSWORD --fail http://localhost:3000/
+  exec curl --user $SECURE_USERNAME:$SECURE_PASSWORD --fail http://localhost:3000/environments
 fi
