d511b1349ab931390dfe71e8c297adc25d1587ea
Peter Fry <peterfry@Peters-MacBook-Pro-2.local>
2019-06-03 14:45:42 +0100

    reenabling all default routes for resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :environments, only: :index
+  resources :environments
+  # resources :environments, only: :index
   patch  '/environmentsByName'      => 'environmentsapi#updateByName'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
